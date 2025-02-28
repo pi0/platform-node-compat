2e273702b9425dacd32bc5de618c11a0ba2e667e
Pooya Parsa <pooya@pi0.io>
2025-02-28 23:30:58 +0100

    try

--- src/veecel.mjs
+++ src/veecel.mjs
@@ -2,22 +2,22 @@ import handler from "./index.mjs";
 
 // https://vercel.com/docs/functions/edge-middleware/edge-runtime#compatible-node.js-modules
 
-import _async_hooks from "node:async_hooks"
-import _events from "node:events"
-import _buffer from "node:buffer"
-import _assert from "node:assert"
-import _util from "node:util"
+// import _async_hooks from "node:async_hooks"
+// import _events from "node:events"
+// import _buffer from "node:buffer"
+// import _assert from "node:assert"
+// import _util from "node:util"
 
 const nodeModules = {
-  "node:async_hooks": _async_hooks,
-  "node:events": _events,
-  "node:buffer": _buffer,
-  "node:assert": _assert,
-  "node:util": _util
+  // "node:async_hooks": _async_hooks,
+  // "node:events": _events,
+  // "node:buffer": _buffer,
+  // "node:assert": _assert,
+  // "node:util": _util
 }
 
 globalThis.getVercelBuiltinModule = (specifier) => nodeModules[specifier]
 
-export default function handler(req) {
+export default function (req) {
   return handler(req);
 }
